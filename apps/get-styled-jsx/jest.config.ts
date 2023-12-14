1829c58f9bebb596c15848525ab42c150f26c6c9
Jakob Nielsen <jakob@frontend-freelancer.com>
2023-12-14 23:46:11 +0100

    Initial commit

---
+++ apps/get-styled-jsx/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'get-styled-jsx',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/get-styled-jsx',
+};
